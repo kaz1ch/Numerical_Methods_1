87b6f8b5db75c56a5a4418ce71de062aeabbf4b5
Илья Казютин <gorgutz1@yandex.ru>
2020-09-18 11:16:56 +0300

    Исправил бесконечное зацикливание и уменьшил шаг графика по x.

--- Numerical_Methods_1/Form1.cs
+++ Numerical_Methods_1/Form1.cs
@@ -17,7 +17,6 @@ namespace Numerical_Methods_1
         public Form1()
         {
             InitializeComponent();
-            this.chart1.Series[0].Points.Clear();
         }
 
         double f(double x)
@@ -47,26 +46,27 @@ namespace Numerical_Methods_1
             double L_Result = 0;
 
             while (i <= 5)
+            {
                 L_Result += f(x_array[i - 1]) * Phi(x, i);
+                i++;
+            }
 
             return L_Result;
-
         }
 
         private void button1_Click(object sender, EventArgs e) 
         {
             double[] x_array = new double[5] { h / 2, h * 3 / 2, h * 5 / 2, h * 9 / 2, h * 13 / 2 };
-            double a = 0, b = 2 * Pi, step = 0.5;
+            double a = 0, b = 2 * Pi, step = 0.1;
             double x = a + step, y;
-            
+            this.chart1.Series[0].Points.Clear();
+
             while (x < b)
             {
                 y = L(x);
                 this.chart1.Series[0].Points.AddXY(x, y);
                 x += step;
             }
-
-
         }
     }
 }
